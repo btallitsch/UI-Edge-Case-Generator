bcad5f97459cfcacebc94ec6c61dc02408f2d2c5
Bill Tallitsch <1752682+btallitsch@users.noreply.github.com>
2026-03-18 15:36:54 -0400

    Delete src/components directory

--- src/components/StressIndicator.jsx
+++
@@ -1,7 +0,0 @@
-export default function StressIndicator({ active }) {
-  return (
-    <div style={{ color: active ? "red" : "gray" }}>
-      {active ? "⚡ Stress Testing Active" : "Idle"}
-    </div>
-  )
-}
